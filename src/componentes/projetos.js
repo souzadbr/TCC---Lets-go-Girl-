75e0a0e283634afefc9264f13b162b320ff90005
souzadbr <deboral.p@hotmail.com>
2021-08-28 15:22:06 -0300

    Finalizando projeto

--- src/componentes/projetos.js
+++ src/componentes/projetos.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import projetos from '../img/1.png'
-import github from '../img/7.png'
-import hospedagem from '../img/8.png'
+
 
 function Projeto() {
   return (
@@ -10,20 +9,91 @@ function Projeto() {
        <img src= {projetos} alt = "projetos"/>
        <h3>Projetos</h3>
         </div>
-        <div className = "titulo">
-        <h4>Aula Bootstrap</h4>
-        </div>
-        <div className = "Linkprojeto">
-        <div>
-        <img src= {github} alt = "github"  />
+        <div id = "titulo">
+        <h4>Projeto Bootstrap</h4>
         </div>
         <div>
-        <img src= {hospedagem} alt = "hospedagem"/>
+        <p><a href="https://souzadbr.github.io/Aula--Bootstrap/" target ="_blank" rel="noreferrer" >Visualizar página</a></p>
         </div>
+        <div>
+        <p><a href="https://github.com/souzadbr/Aula--Bootstrap" target ="_blank" rel="noreferrer" >Código fonte</a></p>
+        </div>
+        <div id = "titulo">
+        <h4>Snake Game</h4>
+        </div>
+        <div>
+        <p><a href="https://souzadbr.github.io/Snake-Game/" target ="_blank" rel="noreferrer" >Visualizar página</a></p>
+        </div>
+        <div>
+        <p><a href="https://github.com/souzadbr/Snake-Game" target ="_blank" rel="noreferrer" >Código fonte</a></p>
+        </div>
+        <div id = "titulo">
+        <h4>Jogo Rick And Morty</h4>
+        </div>
+        <div>
+        <p><a href="https://souzadbr.github.io/jogo-rick-and-morty/" target ="_blank" rel="noreferrer" >Visualizar página</a></p>
+        </div>
+        <div>
+        <p><a href="https://github.com/souzadbr/jogo-rick-and-morty" target ="_blank" rel="noreferrer" >Código fonte</a></p>
+        </div>
+        <div id = "titulo">
+        <h4>Caracteristicas personagens Rick and Morty</h4>
+        </div>
+        <div>
+        <p><a href="https://souzadbr.github.io/caracteristicasPersonagensAPI/" target ="_blank" rel="noreferrer" >Visualizar página</a></p>
+        </div>
+        <div>
+        <p><a href="https://github.com/souzadbr/caracteristicasPersonagensAPI" target ="_blank" rel="noreferrer" >Código fonte</a></p>
+        </div>
+        <div id = "titulo">
+        <h4>Bookflix</h4>
+        </div>
+        <div>
+        <p><a href="https://souzadbr.github.io/BOOKFLIX/" target ="_blank" rel="noreferrer" >Visualizar página</a></p>
+        </div>
+        <div>
+        <p><a href="https://github.com/souzadbr/BOOKFLIX" target ="_blank" rel="noreferrer" >Código fonte</a></p>
+        </div>
+        <div id = "titulo">
+        <h4>Consulta CEP</h4>
+        </div>
+        <div>
+        <p><a href="https://souzadbr.github.io/consultaCEP/" target ="_blank" rel="noreferrer" >Visualizar página</a></p>
+        </div>
+        <div>
+        <p><a href="https://github.com/souzadbr/consultaCEP" target ="_blank" rel="noreferrer" >Código fonte</a></p>
+        </div>
+        <div id = "titulo">
+        <h4>Desafio Estrelas Fora da Caixa</h4>
+        </div>
+        <div>
+        <p><a href="https://souzadbr.github.io/desafio-estrelas-/" target ="_blank" rel="noreferrer" >Visualizar página</a></p>
+        </div>
+        <div>
+        <p><a href="https://github.com/souzadbr/desafio-estrelas-" target ="_blank" rel="noreferrer" >Código fonte</a></p>
+        </div>
+        <div id = "titulo">
+        <h4>Clone Instagram</h4>
+        </div>
+        <div>
+        <p><a href="https://souzadbr.github.io/insta-clone/" target ="_blank" rel="noreferrer" >Visualizar página</a></p>
+        </div>
+        <div>
+        <p><a href="https://github.com/souzadbr/insta-clone" target ="_blank" rel="noreferrer" >Código fonte</a></p>
+        </div>
+        <div id = "titulo">
+        <h4>Portfólio</h4>
+        </div>
+        <div>
+        <p><a href="https://souzadbr.github.io/Portfolio/" target ="_blank" rel="noreferrer" >Visualizar página</a></p>
+        </div>
+        <div>
+        <p><a href="https://github.com/souzadbr/Portfolio" target ="_blank" rel="noreferrer" >Código fonte</a></p>
         </div>
         
+  
     </div>
   );
-}
+  }
 
 export default Projeto;
